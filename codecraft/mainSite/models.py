962451661e5f973bc62755ea685152f8741c41ef
dmfadul <dmf030@gmail.com>
2025-02-06 15:25:42 -0300

    migrated models

--- codecraft/mainSite/models.py
+++ codecraft/mainSite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Message(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_phone = models.CharField(max_length=50)
+    user_email = models.CharField(max_length=50)
+    user_message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_replied = models.BooleanField(default=False)
