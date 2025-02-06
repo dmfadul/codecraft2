7a76e91de33683c03c6154661b43dca2b0664af1
dmfadul <dmf030@gmail.com>
2025-02-06 17:15:10 -0300

    added message creation

--- codecraft/mainSite/views.py
+++ codecraft/mainSite/views.py
@@ -16,5 +16,21 @@ def plans(request):
     return render(request, 'mainSite/plans.html')
 
 def submit(request):
-    print("TESTE DE SUBMISSÃO")
-    return render(request, 'mainSite/about.html')
+    from .models import Message
+
+    data_dict = request.POST
+
+    user_name = data_dict.get('nome')
+    user_phone = data_dict.get('telefone')
+    user_email = data_dict.get('email')
+    user_message = data_dict.get('pedido')
+
+    new_message = Message.objects.create(
+        user_name = user_name,
+        user_phone = user_phone,
+        user_email = user_email,
+        user_message = user_message,
+    )
+
+
+    return render(request, 'mainSite/index.html')
