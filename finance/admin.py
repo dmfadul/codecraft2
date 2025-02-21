908f040eb470764f03e954a60edcfef924dd6640
dmfadul <dmf030@gmail.com>
2025-02-21 16:46:32 -0300

    registered models

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Card, Expense, Payment, CardTransaction
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Card)
+admin.site.register(Expense)
+admin.site.register(Payment)
+admin.site.register(CardTransaction)
