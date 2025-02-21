bc35d1b76acc60ffc8a9117520f675c739685726
dmfadul <dmf030@gmail.com>
2025-02-21 16:29:13 -0300

    added null=true to set_null models

--- finance/models.py
+++ finance/models.py
@@ -20,7 +20,10 @@ class Card(models.Model):
     cvv = models.CharField(max_length=3, blank=True, null=True)
     expiry = models.DateField(blank=True, null=True)
 
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='cards')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 related_name='cards')
 
     def __str__(self):
         return self.title
@@ -33,7 +36,10 @@ class Expense(models.Model):
     exp_amount = models.DecimalField(max_digits=10, decimal_places=2)
     is_fixed = models.BooleanField(default=False)
 
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='expenses')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 related_name='expenses')
 
     def __str__(self):
         return self.title
@@ -49,9 +55,15 @@ class Payment(models.Model):
 
 
 class CardTransaction(models.Model):
-    card = models.ForeignKey(Card, on_delete=models.SET_NULL, related_name='transactions')
+    card = models.ForeignKey(Card,
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             related_name='transactions')
     description = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="card_transactions")
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 related_name="card_transactions")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     bill_month = models.DateField()
