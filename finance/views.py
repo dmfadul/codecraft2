5addb2128f4a14c9a4bf49067f41e68be47b4320
dmfadul <dmf030@gmail.com>
2025-02-24 16:35:35 -0300

    adjusted routes and views

--- finance/views.py
+++ finance/views.py
@@ -20,26 +20,30 @@ class CategoryCreateView(CreateView):
     template_name = 'finance/category_form.html'
     success_url = reverse_lazy('add-category')  # or wherever you want to redirect
 
+
 class CardCreateView(CreateView):
     model = Card
     fields = ['title', 'bank', 'banner', 'limit', 'number', 'cvv', 'expiry', 'category']
-    template_name = 'expense_app/card_form.html'
+    template_name = 'finance/card_form.html'
     success_url = reverse_lazy('add-card')
+
 
 class ExpenseCreateView(CreateView):
     model = Expense
     fields = ['title', 'description', 'due_date', 'exp_amount', 'is_fixed', 'category']
-    template_name = 'expense_app/expense_form.html'
+    template_name = 'finance/expense_form.html'
     success_url = reverse_lazy('add-expense')
+
 
 class PaymentCreateView(CreateView):
     model = Payment
     fields = ['expense', 'month', 'amount', 'is_paid', 'date', 'obs']
-    template_name = 'expense_app/payment_form.html'
+    template_name = 'finance/payment_form.html'
     success_url = reverse_lazy('add-payment')
+
 
 class CardTransactionCreateView(CreateView):
     model = CardTransaction
     fields = ['card', 'description', 'category', 'amount', 'date', 'bill_month', 'number_of_stallments']
-    template_name = 'expense_app/card_transaction_form.html'
+    template_name = 'finance/card_transaction_form.html'
     success_url = reverse_lazy('add-card-transaction')
