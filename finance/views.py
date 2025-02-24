6f30b876f4cff8376bfc7e27dbca69df04ff777d
dmfadul <dmf030@gmail.com>
2025-02-24 17:21:49 -0300

    added login and logout to dashboard

--- finance/views.py
+++ finance/views.py
@@ -18,28 +18,28 @@ class CategoryCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('add-category')  # or wherever you want to redirect
 
 
-class CardCreateView(CreateView):
+class CardCreateView(LoginRequiredMixin, CreateView):
     model = Card
     fields = ['title', 'bank', 'banner', 'limit', 'number', 'cvv', 'expiry', 'category']
     template_name = 'finance/card_form.html'
     success_url = reverse_lazy('add-card')
 
 
-class ExpenseCreateView(CreateView):
+class ExpenseCreateView(LoginRequiredMixin, CreateView):
     model = Expense
     fields = ['title', 'description', 'due_date', 'exp_amount', 'is_fixed', 'category']
     template_name = 'finance/expense_form.html'
     success_url = reverse_lazy('add-expense')
 
 
-class PaymentCreateView(CreateView):
+class PaymentCreateView(LoginRequiredMixin, CreateView):
     model = Payment
     fields = ['expense', 'month', 'amount', 'is_paid', 'date', 'obs']
     template_name = 'finance/payment_form.html'
     success_url = reverse_lazy('add-payment')
 
 
-class CardTransactionCreateView(CreateView):
+class CardTransactionCreateView(LoginRequiredMixin, CreateView):
     model = CardTransaction
     fields = ['card', 'description', 'category', 'amount', 'date', 'bill_month', 'number_of_stallments']
     template_name = 'finance/card_transaction_form.html'
