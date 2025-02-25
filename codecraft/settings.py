88c1435b32ef147cb86d3898241b3d6782f9721f
dmfadul <dmf030@gmail.com>
2025-02-25 15:09:23 -0300

    moved around static files

--- codecraft/settings.py
+++ codecraft/settings.py
@@ -130,10 +130,9 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
-if getenv("IS_DEVELOPMENT") == "False":
-    STATICFILES_DIRS = []
-else:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
