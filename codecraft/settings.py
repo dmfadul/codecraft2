5f0c3d8ea824a2b4a73f98bfa57122e3fd98896f
doctormatterhorn <dmf030@gmail.com>
2025-03-01 19:29:08 -0300

    added a secret secret settings file to .gitignore and altered settings to use it

--- codecraft/settings.py
+++ codecraft/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 try:
-    from .settings_secrets import *
+    from .settings_secret import *
 except ImportError:
     raise Exception("Missing settings_secret.py file!")
 
