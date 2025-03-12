019647d3298423e655c10e19ce06626b3e77a6e8
dmfadul <dmf030@gmail.com>
2025-03-12 16:07:12 -0300

    added Context to poker models

--- poker/models.py
+++ poker/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Context(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.abbreviation
+    
+
 class Position(models.Model):
     name = models.CharField(max_length=20, unique=True)
 
@@ -8,10 +17,11 @@ class Position(models.Model):
 
 
 class RangeEntry(models.Model):
+    stack = models.IntegerField(default=100)  # Stack size in BBs
+    # context = models.ForeignKey(Context, on_delete=models.CASCADE)
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
     hand = models.CharField(max_length=5)  # e.g., "AKs", "QJo"
     action = models.CharField(max_length=5, choices=[('raise', 'Raise'), ('limp', 'Limp')])
-    stack = models.IntegerField(default=100)  # Stack size in BBs
 
     class Meta:
         unique_together = ('position', 'hand', 'stack')  # Avoid duplicate entries
